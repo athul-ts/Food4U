5c30d4866ccd24e353cb52df285c8200f9a3be14
Áthul T S <sanuchandra90@gmai.com>
2023-12-28 19:29:37 +0530

    v1.0015

--- asset/script.js
+++ asset/script.js
@@ -1,30 +1,44 @@
-document.addEventListener("DOMContentLoaded", function(){
+// document.addEventListener("DOMContentLoaded", function(){
 
-    el_autohide = document.querySelector('.autohide');
+//     el_autohide = document.querySelector('.autohide');
     
-    // add padding-top to bady (if necessary)
-    navbar_height = document.querySelector('.navbar').offsetHeight;
-    // document.body.style.paddingTop = navbar_height + 'px';
+//     // add padding-top to bady (if necessary)
+//     navbar_height = document.querySelector('.navbar').offsetHeight;
+//     // document.body.style.paddingTop = navbar_height + 'px';
   
-    if(el_autohide){
-      var last_scroll_top = 0;
-      window.addEventListener('scroll', function() {
-            let scroll_top = window.scrollY;
-           if(scroll_top < last_scroll_top) {
-                el_autohide.classList.remove('scrolled-down');
-                el_autohide.classList.add('scrolled-up');
-                // el_autohide.classList.add('bg-dark', 'shadow');
-            }
-            else {
-                el_autohide.classList.remove('scrolled-up');
-                el_autohide.classList.add('scrolled-down');
-                // el_autohide.classList.remove('bg-dark', 'shadow');
-            }
-            last_scroll_top = scroll_top;
-      }); 
-      // window.addEventListener
-    }
-    // if
+//     if(el_autohide){
+//       var last_scroll_top = 0;
+//       window.addEventListener('scroll', function() {
+//             let scroll_top = window.scrollY;
+//            if(scroll_top < last_scroll_top) {
+//                 el_autohide.classList.remove('scrolled-down');
+//                 el_autohide.classList.add('scrolled-up');
+//                 // el_autohide.classList.add('bg-dark', 'shadow');
+//             }
+//             else {
+//                 el_autohide.classList.remove('scrolled-up');
+//                 el_autohide.classList.add('scrolled-down');
+//                 // el_autohide.classList.remove('bg-dark', 'shadow');
+//             }
+//             last_scroll_top = scroll_top;
+//       }); 
+//       // window.addEventListener
+//     }
+//     // if
   
-  }); 
-  // DOMContentLoaded  end
+//   }); 
+//   // DOMContentLoaded  end
+
+
+
+
+
+  var nav = document.querySelector('.navbar');
+
+      window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 100) {
+          nav.classList.add('bg-white', 'shadow');
+        } else {
+          nav.classList.remove('bg-white', 'shadow');
+        }
+      });
